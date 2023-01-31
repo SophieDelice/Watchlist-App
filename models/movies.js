0dde408eadd287d1f7114221e06a3bdd681a6a3a
Sophie Delice <sophie@Sophies-MacBook-Air.local>
2023-01-30 21:09:26 -0500

    wip: adds filesystem

--- models/movies.js
+++ models/movies.js
@@ -1,1 +1,14 @@
-// Page for my schema 
+const mongoose = require("mongoose"); 
+
+const Schema = mongoose.Schema; 
+
+
+const moviesSchema = new Schema({
+  category: {type: String},
+  title: {type: String, required: true },
+  streamingService: {type: String, }, 
+  year: { type: Date, required: true}, 
+  watched: Boolean
+});
+
+module.exports = mongoose.model("Movies", moviesSchema); 
--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const app = express();
 
 // configure settings 
 require("dotenv").config(); 
-const PORT = process.env.PORT;
 
- 
+const PORT = process.env.PORT;
 
 const DATABASE_URL = process.env.DATABASE_URL; 
 
 
 //establish conenction to mongodb 
+mongoose.set("strictQuery", false);
 mongoose.connect(DATABASE_URL); 
 
 const db = mongoose.connection; 
@@ -28,11 +28,36 @@ db.on("connected", () => {
 db.on("error" , (error) => {
     console.log("An error occurred with MongoDB:" + error.message);
 }); 
-// mount middleware 
 
+// mount middleware 
+app.use(express.urlencoded({ extended: false})); 
 
 // mount routes 
 
+// INDUCES 
+
+// Index 
+
+// New 
+
+// Delete 
+
+// Update 
+
+// Create
+app.post("/movies", (req, res) => {
+    if(req.body.watched === "on") {
+        req.body.watched = true; 
+    } else {
+        req.body.watched = false; 
+    }
+  res.send(req.body);
+  
+}); 
+
+// Edit
+
+// Show 
 
 // tell the app to listen 
 app.listen(PORT, () => {
